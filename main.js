82d623dfc3a0ad70f04b254b311c89181a607476
InesGaliza <ines.galiza2018r@gmail.com>
2024-01-21 14:42:47 +0000

    botoes_a_funcionar

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
-console.log("hellooooooo")
-
 //MENU
 //ANIMAÇÃO
 
 //AO CLICAR NO BOTÃO VET ELE FICA ROXO COM LETRAS BRANCAS
 
-    var menuVetroxo = document.getElementById("vet_roxo");
-    var menuVetwhite = document.getElementById("vet_branco");
-    var menuButton = document.getElementById("button_vet");
-    
-    menuButton.addEventListener("click", function() {
-      console.log("clicked")
-      menuVetwhite.style.display = "block"; 
-      menuVetroxo.style.display = "none";
-      document.getElementById("link_vet").style.color = "#fff";
-    });
+  var menuVetroxo = document.getElementById("vet_roxo");
+  var menuVetwhite = document.getElementById("vet_branco");
+  var menuButton = document.getElementById("button_vet");
+  var menuButtonNao = document.getElementById("button_n_selecionado");
+  
+  menuButton.addEventListener("touchstart", function() {
+    console.log("clicked")
+    menuVetwhite.style.display = "block"; 
+    menuVetroxo.style.display = "none";
+    document.getElementById("link_vet").style.color = "#fff";
+  });
+
+  menuButton.addEventListener("touchend", function() {
+    location.href = "./veterinario.html"
+  });
+
+  menuButtonNao.addEventListener("touchstart", function(){
+    console.log("clicked")
+    menuVetwhite.style.display = "block"; 
+    menuVetroxo.style.display = "none";
+    document.getElementById("link_vet").style.color = "#fff";
+  })
+
+  menuButtonNao.addEventListener("touchend", function() {
+    location.href = "./index.html"
+  });
